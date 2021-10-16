8517a5a35e81edf59166531fc6300003adc68d9e
sunny <sunny@sunnymac.local>
2021-10-16 11:09:32 +0800

    Clean main.js, fix icp

--- themes/draft/static/js/main.js
+++ themes/draft/static/js/main.js
@@ -1,22 +1,2 @@
 window.addEventListener('DOMContentLoaded', function () {
-	const LIGHT = 'light', DARK = 'dark';
-	const themeSwitcher = document.getElementById('theme-switcher');
-
-	themeSwitcher.addEventListener('click', function () {
-		changeTheme(true);
-	});
-
-	function changeTheme(isToggle) {
-		const currentTheme = localStorage.theme,
-			{ classList } = document.querySelector('html');
-		var newTheme = currentTheme;
-		if (isToggle) {
-			newTheme = currentTheme === LIGHT ? DARK : LIGHT;
-		}
-		classList.remove(currentTheme);
-		classList.add(newTheme);
-		localStorage.theme = newTheme;
-	}
-
-	changeTheme();
 });
